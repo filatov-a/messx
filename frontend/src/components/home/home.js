d2c0e61e1403dc742ce705754060ee52627b4eb1
Artem Filatov <59095439+filatov-a@users.noreply.github.com>
2022-05-08 18:31:55 +0200

    home

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -3,6 +3,7 @@ import { Box, Typography } from '@mui/material';
 import {makeStyles} from '@mui/styles'
 import {useTranslation} from 'react-i18next'
 import {Link} from "react-router-dom"
+import config from "../../config/config";
 const Tr = useTranslation;
 
 const UseStyles = makeStyles({
@@ -17,6 +18,15 @@ function home() {
     const {t} = Tr();
     return (
         <Box>
+            <Box display={'flex'} style={{width: "100%", backgroundRepeat: "repeat-x"}}>
+                <img style={{width: 150}} src={`${config.url}/images/Earth.png`} alt={''}/>
+                <div style={{color: "gray"}}>
+                    <p>      # 0 1 0 1      </p>
+                    <p># 0 1 0 1 0 0 1 0 1 #</p>
+                    <p># 0 1 0 1 0 0 1 0 1 #</p>
+                    <p>      # 0 1 0 1      </p>
+                </div>
+            </Box>
             <Typography className={classes.text} variant="h4" component="h2">
                 {t("hello")}
             </Typography>
